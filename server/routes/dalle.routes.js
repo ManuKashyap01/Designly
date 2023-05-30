7b5b691c0d5e777bcd7341d424fc620e986a2360
ManuKashyap01 <kashyap.manu200201@gmail.com>
2023-05-30 10:17:52 +0530

    dalle routes

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import {Configuration,OpenAIApi} from 'openai'
+import fetch from 'node-fetch'
+// import {Configuration,OpenAIApi} from 'openai'
 
 dotenv.config()
 
 // router is used to create different routes
 const router=express.Router()
 
-const config=new Configuration({
-    apiKey:process.env.API_KEY
-})
+// const config=new Configuration({
+//     apiKey:process.env.API_KEY
+// })
 // create an instance of openai by using the config
-const openapi=new OpenAIApi(config)
+// const openapi=new OpenAIApi(config)
 
 router.route('/').get((req,res)=>{
     res.status(200).json({message:'Hello from Dalle routes'})
@@ -20,13 +21,32 @@ router.route('/').get((req,res)=>{
 router.route('/').post(async (req,res)=>{
     try {
         const {prompt}=req.body
-        const response=await openapi.createImage({
-            prompt,
-            n:1,
-            size:'1024x1024',
-            response_format:'b64_json'
-        })
-        const image=response.data.data[0].b64_json
+        // const response=await openapi.createImage({
+        //     prompt,
+        //     n:1,
+        //     size:'1024x1024',
+        //     response_format:'b64_json'
+        // })
+        const response=await fetch(
+            `https://api.edenai.run/v2/image/generation`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.EDENAI_API_KEY}`
+              },
+              body: JSON.stringify({
+                providers: 'openai',
+                text: prompt,
+                resolution: '512x512',
+                num_images: 1,
+                response_format:'b64_json'
+              })
+            }
+          );
+        const data = await response.json();
+        const image=data.openai.items[0].image
+        console.log(data.openai.items)
         res.status(200).json({photo:image})
     } catch (error) {
         console.log(error)
